a8e4b2106ee43788f8935e07e0aada3f96ebef1b
RandomowyRandom <38404029+RandomowyRandom@users.noreply.github.com>
2021-12-04 23:42:37 +0100

    added reactjs-poput, project modals and actual projects

---
+++ src/Project.ts
@@ -0,0 +1,6 @@
+class Project{
+    constructor(public name:string, public description:string, public image:string, public link:string){}
+}
+
+
+export default Project;
--- src/components/ProjectHolder.tsx
+++ src/components/ProjectHolder.tsx
@@ -1,26 +1,40 @@
 import { Component } from "react";
+import { Popup } from "reactjs-popup";
+import ProjectModal from "./ProjectModal";
+import Project from "../Project";
 import "../styles/ProjectHolder.css";
 
 interface IProps {
-    projectName: string;
-    projectImage?: string;
+    project: Project;
 }
 
 interface IState {
-
+    isOpen: boolean;
 }
 
 class ProjectHolder extends Component<IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
     render() {
         return (
             <div className="project-holder">
-                <h3 className="project-name">{this.props.projectName}</h3>
+                <h3 className="project-name">{this.props.project.name}</h3>
                 <div className="image-holder">
-                    <img src={this.props.projectImage} alt="alt" />
+                <img onClick={() => this.setState({isOpen: true})} src={this.props.project.image} alt="project cover"/>
+                    <Popup open={this.state.isOpen} modal>
+                        <ProjectModal onCloseButtonPressed={() => this.setState({isOpen: false})} project={this.props.project}/>    
+                    </Popup> 
                 </div>
             </div>
         )
     }
 }
+
 
 export default ProjectHolder;
