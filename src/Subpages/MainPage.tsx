7287b0d29ec345e4eaafb707cbee44b29ba60c1c
RandomowyRandom <domi20100501@gmail.com>
2022-01-13 21:05:33 +0100

    Finished main page

--- src/Subpages/MainPage.tsx
+++ src/Subpages/MainPage.tsx
@@ -1,6 +1,11 @@
 import { Component } from "react";
 import AboutMeQuestion from "../components/AboutMeQuestion";
 import "../styles/MainPage.css";
+import gif1 from "../Screenshots/GIF_1.gif";
+import gif2 from "../Screenshots/GIF_2.gif";
+import gif3 from "../Screenshots/GIF_3.gif";
+import screenshot1 from "../Screenshots/Screenshot_1.png";
+import screenshot2 from "../Screenshots/Screenshot_2.png";
 
 interface IProps {}
 
@@ -25,12 +30,13 @@ class MainPage extends Component<IProps, IState> {
         <div className="welcome">
           <h1 className="welcome-text">Welcome traveller!</h1>
           <h2 className="welcome-subtext">Take a quick look around!</h2>
+          {this.line}
         </div>
 
         <div className="main-content">
           <div className="about-me">
+            <h1>About me</h1>
             {this.line}
-
             <AboutMeQuestion
               question="Who Am I?"
               answer="My name is Dominik and I am a 17 years old student passionate
@@ -71,6 +77,23 @@ class MainPage extends Component<IProps, IState> {
                 look at the rest of the site, it's pretty cool and you might
                 find answers there!"
             ></AboutMeQuestion>
+          </div>
+          <div className="highlights">
+            <h1>Highlighted project</h1>
+            {this.line}
+            <h2>Friendship Exterminator</h2>
+            <p>
+              <b>Friendship Exterminator</b> is an upcoming multiplayer rage
+              game with procedurally generated map, where your goal is to reach
+              the top of the dungeon while befriending or betraying as many of
+              your friends as you can.
+            </p>
+            {this.line}
+            <img src={gif1} alt="game gif" />
+            <img src={gif2} alt="game gif" />
+            <img src={gif3} alt="game gif" />
+            <img src={screenshot1} alt="game screenshot" />
+            <img src={screenshot2} alt="game screenshot" />
           </div>
         </div>
       </div>
